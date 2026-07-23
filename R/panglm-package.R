a40847157d92afcc1bcd2ae4fce043303924c8ff
ielbadisy <elbadisyimad@gmail.com>
2026-07-23 01:43:27 +0100

    Add package-level doc with useDynLib/importFrom directives

---
+++ R/panglm-package.R
@@ -0,0 +1,8 @@
+#' panglm: Fast Generalized Linear Models for Panel Data
+#'
+#' @keywords internal
+#' @useDynLib panglm, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @importFrom data.table as.data.table setDT setkeyv
+#' @importFrom stats model.frame model.matrix model.response pnorm printCoefmat nobs
+"_PACKAGE"
